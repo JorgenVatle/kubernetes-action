7e1576cc63a09c99dbb203bdb86a7f3cb6031b4f
Jørgen Vatle <Jorgen@WcAServices.net>
2022-07-19 17:29:07 +0300

    Capture and format error and result fields.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,19 @@
 echo ${KUBE_CONFIG_DATA} | base64 -d > kubeconfig
 export KUBECONFIG=kubeconfig
 
-result="$(kubectl $1 2>&1)"
+result="$(kubectl $1 2> error.log)"
+result="${result//'%'/'%25'}"
+result="${result//$'\n'/'%0A'}"
+result="${result//$'\r'/'%0D'}"
+echo ::set-output name=result::"$result"
+echo "$result"
+
+error="$(cat error.log)"
+error="${error//'%'/'%25'}"
+error="${error//$'\n'/'%0A'}"
+error="${error//$'\r'/'%0D'}"
+echo ::set-output name=error::"$error"
+echo "$error"
 
 status=$?
-echo ::set-output name=result::$result
-echo "$result"
 if [[ $status -eq 0 ]]; then exit 0; else exit 1; fi
