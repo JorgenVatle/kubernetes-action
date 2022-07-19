42d43a59d5a5f4bc3afb6ab434c10afeb2a50d6c
Jørgen Vatle <Jorgen@WcAServices.net>
2022-07-19 18:42:50 +0300

    Revert "Simplify output from entrypoint.sh"
    This reverts commit e36b0e8e

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,20 @@
 echo ${KUBE_CONFIG_DATA} | base64 -d > kubeconfig
 export KUBECONFIG=kubeconfig
 
-result="$(kubectl $1 2>&1)"
+result="$(kubectl $1 2> error.log)"
 status=$?
 
-echo "::set-output name=result::$result"
+result="${result//'%'/'%25'}"
+result="${result//$'\n'/'%0A'}"
+result="${result//$'\r'/'%0D'}"
+echo ::set-output name=result::"$result"
 echo "$result"
+
+error="$(cat error.log)"
+error="${error//'%'/'%25'}"
+error="${error//$'\n'/'%0A'}"
+error="${error//$'\r'/'%0D'}"
+echo ::set-output name=error::"$error"
+echo "$error"
 
 if [[ $status -eq 0 ]]; then exit 0; else exit 1; fi
