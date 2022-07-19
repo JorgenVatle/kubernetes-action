e36b0e8ec1f4003d5206c681b506bfe405b17dc1
Jørgen Vatle <Jorgen@WcAServices.net>
2022-07-19 18:01:48 +0300

    Simplify output from entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,17 +6,10 @@ export KUBECONFIG=kubeconfig
 result="$(kubectl $1 2> error.log)"
 status=$?
 
-result="${result//'%'/'%25'}"
-result="${result//$'\n'/'%0A'}"
-result="${result//$'\r'/'%0D'}"
-echo ::set-output name=result::"$result"
+echo "::set-output name=result::$result"
 echo "$result"
 
-error="$(cat error.log)"
-error="${error//'%'/'%25'}"
-error="${error//$'\n'/'%0A'}"
-error="${error//$'\r'/'%0D'}"
-echo ::set-output name=error::"$error"
+echo "::set-output name=error::$(cat error.log)"
 echo "$error"
 
 if [[ $status -eq 0 ]]; then exit 0; else exit 1; fi
