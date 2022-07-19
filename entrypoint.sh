4bc6bca6cd7bd67c197205879ce3634e0ace1484
Jørgen Vatle <Jorgen@WcAServices.net>
2022-07-19 17:33:43 +0300

    Correct status input in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ echo ${KUBE_CONFIG_DATA} | base64 -d > kubeconfig
 export KUBECONFIG=kubeconfig
 
 result="$(kubectl $1 2> error.log)"
+status=$?
+
 result="${result//'%'/'%25'}"
 result="${result//$'\n'/'%0A'}"
 result="${result//$'\r'/'%0D'}"
@@ -17,5 +19,4 @@ error="${error//$'\r'/'%0D'}"
 echo ::set-output name=error::"$error"
 echo "$error"
 
-status=$?
 if [[ $status -eq 0 ]]; then exit 0; else exit 1; fi
