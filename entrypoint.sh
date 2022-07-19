c2341b2d932bb27df1a07abf0fa5f423db644054
Jørgen Vatle <Jorgen@WcAServices.net>
2022-07-19 18:04:22 +0300

    Revert to older output capture structure.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,10 @@
 echo ${KUBE_CONFIG_DATA} | base64 -d > kubeconfig
 export KUBECONFIG=kubeconfig
 
-result="$(kubectl $1 2> error.log)"
+result="$(kubectl $1)"
 status=$?
 
 echo "::set-output name=result::$result"
 echo "$result"
-
-echo "::set-output name=error::$(cat error.log)"
-echo "$error"
 
 if [[ $status -eq 0 ]]; then exit 0; else exit 1; fi
