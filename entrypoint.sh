90218ac20551118f59a67840b66786f828dc41d2
Jørgen Vatle <Jorgen@WcAServices.net>
2022-07-19 18:07:07 +0300

    Capture stderr in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 echo ${KUBE_CONFIG_DATA} | base64 -d > kubeconfig
 export KUBECONFIG=kubeconfig
 
-result="$(kubectl $1)"
+result="$(kubectl $1 2>&1)"
 status=$?
 
 echo "::set-output name=result::$result"
